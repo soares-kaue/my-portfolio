685d50eb6b71e2fd718d342fb8052b9f1bdd59af
Kauê Soares <home.kauesoares@gmail.com>
2026-08-28 00:21:33 -0300

    Fix Errors

--- scripts.js
+++ scripts.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
 window.addEventListener('beforeunload', () => {
-    localStorage.setItem('scrollPos', window.scrollY);
-});
+    localStorage.setItem('scrollPos', window.scrollY)
+})
 
-// Restaura a posição após carregar
-window.addEventListener('load', () => {
-    const scrollPos = localStorage.getItem('scrollPos');
-    if (scrollPos) {
-        window.scrollTo(0, parseInt(scrollPos));
-    }
-=======
-window.addEventListener('beforeunload', () => {
-    localStorage.setItem('scrollPos', window.scrollY);
-});
 
-// Restaura a posição após carregar
 window.addEventListener('load', () => {
-    const scrollPos = localStorage.getItem('scrollPos');
+    const scrollPos = localStorage.getItem('scrollPos')
     if (scrollPos) {
-        window.scrollTo(0, parseInt(scrollPos));
+        window.scrollTo(0, parseInt(scrollPos))
     }
->>>>>>> fedf7a21f9a6a1cd2c745b01736d2749e6f9bc19
-});
+})
